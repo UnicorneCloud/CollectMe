b0add7bec8a283b07e2256bc068ad0e16eb6cfc1
Alexandre Tremblay <alexandre.tremblay@unicornpowered.com>
2019-03-08 21:56:18 -0500

    object instead of map

--- src/utils/__test__/model.test.js
+++ src/utils/__test__/model.test.js
@@ -11,20 +11,16 @@ describe("When creating the frequence", () => {
   beforeEach(() => {
     myFrequence = createFrequence(DAYS_OF_THE_WEEK, 1);
   });
-
-  it("should by a map", () => {
-    expect(myFrequence.type).toBe(Map.prototype.type);
-  });
-
+  
   it("should by a set for 'days'", () => {
-    expect(myFrequence.get("days").type).toBe(Set.prototype.type);
+    expect(myFrequence.days.type).toBe(Set.prototype.type);
   });
 
   it("should create the days in the 'days' key", () => {
-    expect(myFrequence.get("days")).toEqual(EXPECTED_FREQUENCE.get("days"));
+    expect(myFrequence.days).toEqual(EXPECTED_FREQUENCE.get("days"));
   });
 
   it("should return 1 as a period", () => {
-    expect(myFrequence.get("period")).toBe(EXPECTED_FREQUENCE.get("period"));
+    expect(myFrequence.period).toBe(EXPECTED_FREQUENCE.get("period"));
   });
 });
--- src/utils/model.js
+++ src/utils/model.js
@@ -1,8 +1,8 @@
 export const DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 export const createFrequence = (iterableOfDay, everyXWeek) => {
-  return new Map([
-    ["days", new Set(iterableOfDay)],
-    ["period", everyXWeek]
-  ])
+  return {
+    days: new Set(iterableOfDay),
+    period: everyXWeek
+  };
 };
