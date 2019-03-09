a69623bb51ce50ca62f7c60fe1385c375263c7d2
Alexandre Tremblay <alexandre.tremblay@unicornpowered.com>
2019-03-09 12:47:40 -0500

    little text change in astuce

--- src/screens/Walkthrough/data.js
+++ src/screens/Walkthrough/data.js
@@ -11,27 +11,27 @@ export const DATA = [
 },
 {
 	"id": 1,
-	"headline": "Avoir toujours des ustensils dans son sac!",
+	"headline": "Toujours avoir des ustensiles dans son sac!",
 	"icon": <MaterialCommunityIcons name="silverware-fork" style={styles.imageIcons} />
 },
 {
 	"id": 2,
-	"headline": "Pensez à acheter en vrac et local!",
+	"headline": "Acheter local et en vrac!",
 	"icon": <MaterialCommunityIcons name="truck-fast" style={styles.imageIcons} />
 },
 {
 	"id": 3,
-	"headline": "Pensez réutilisable et non jetable!",
+	"headline": "Penser réutilisable et non jetable!",
 	"icon": <MaterialCommunityIcons name="recycle" style={styles.imageIcons} />
 },
 {
 	"id": 4,
-	"headline": "Fabriquer son rince-bouche maison!",
+	"headline": "Fabriquer son propre rince-bouche maison!",
 	"icon": <MaterialCommunityIcons name="spray" style={styles.imageIcons} />
 },
 {
 	"id": 5,
-	"headline": "Raporter ses contenants non utilisé à son épicerie en vrac!",
+	"headline": "Faire son épicerie en vrac avec ses vieux contenants!",
 	"icon": <MaterialIcons name="local-grocery-store" style={styles.imageIcons}  />
 }
 ];
