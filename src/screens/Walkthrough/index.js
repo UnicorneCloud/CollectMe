07ad590c92e9b05b10cb2eac5a92b1e3cb4d0f9e
Eric Pinet <eric.pinet@unicornpowered.com>
2019-03-09 13:03:31 -0500

    WIP

--- src/screens/Walkthrough/index.js
+++ src/screens/Walkthrough/index.js
@@ -11,6 +11,9 @@ import datas from "./data.json"
 import styles from "./styles";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
+
+import { Permissions, Notifications } from 'expo'
+
 const deviceHeight = Dimensions.get("window").height;
 const deviceWidth = Dimensions.get("window").width;
 
@@ -28,10 +31,58 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
+
+async function registerForPushNotificationsAsync() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  console.log('#1')
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+  console.log('#2')
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+  console.log('#3')
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+  console.log('TOKEN: '+ token)
+
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
+  // return fetch(PUSH_ENDPOINT, {
+  //   method: 'POST',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     token: {
+  //       value: token,
+  //     },
+  //     user: {
+  //       username: 'Brent',
+  //     },
+  //   }),
+  // });
+}
 
 class Walkthrough extends Component {
 
   componentDidMount() {
+    registerForPushNotificationsAsync()
     this.props.fetchData(datas)
   }
 
