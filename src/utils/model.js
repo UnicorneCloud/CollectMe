b0433019a96d36f5486064bb4d011820903ed4c0
Hugo Boisselle <hugo.boisselle@gmail.com>
2019-03-09 13:41:07 -0500

    GetNextCollectDates

--- src/utils/model.js
+++ src/utils/model.js
@@ -1,6 +1,7 @@
-import { getFrequence } from '../utils/GeoUtils'
 import GeoJsonGeometriesLookup from 'geojson-geometries-lookup'
+import dayjs from 'dayjs'
 
+import { getFrequence } from '../utils/GeoUtils'
 export const WEEK_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 export const WEEK_DAYS_MAP = {
@@ -29,8 +30,8 @@ export const WEEK_DAYS_IN_DATA = {
 };
 
 export const COLLECT_TYPES = {
-  GARBAGE: 'Garbage',
-  RECYCLING: 'Recycling'
+  GARBAGE: 'déchets',
+  RECYCLING: 'recyclage'
 }
 
 export const createFrequence = (iterableOfDay, everyXWeek) => {
@@ -41,8 +42,8 @@ export const createFrequence = (iterableOfDay, everyXWeek) => {
 };
 
 export const getCollectScheduleData = (location) => {
-  location.latitude = 46.8115809
-  location.longitude = -71.2335447
+  location.latitude = 46.8784799
+  location.longitude = -71.348191
   const locationPoint = { type: 'Point', coordinates: [location.longitude, location.latitude] };
   const collectScheduleDataByType = [
     [COLLECT_TYPES.GARBAGE, require('../../data/dechets.json')], 
@@ -58,3 +59,36 @@ export const getCollectScheduleData = (location) => {
     return result
   }, {})
 }
+
+/**
+ * 
+ * @param {Object} collectSchedule { days: Set, startDate: Date, period: Number }
+ * @returns Next collect days generator
+ * }
+ */
+export function* upcomingCollectDates (collectSchedule) {
+  const now = dayjs()
+  const startDate = dayjs(collectSchedule.startDate)
+  const weeksSinceCollectStart = now.diff(startDate, 'week')
+  const weeksSinceLastCollect = weeksSinceCollectStart % collectSchedule.period
+  const weeksUntilNextCollect = period - weeksSinceLastCollect
+  let nextCollectDay = now.add(weeksUntilNextCollect, 'week').set('day', 0)
+
+  while(true){
+    for(day in collectSchedule.days){
+      nextCollectDay = nextCollectDay.set('day', day)
+      yield nextCollectDay
+    }
+    nextCollectDay.add(collectSchedule.period, 'week')
+  }
+}
+
+export const getUpcomingCollectDates = (collectSchedules, count = 14) => {
+  const upcomingCollectDates = []
+  const upcomingCollectDatesGenerator = upcomingCollectDates(collectSchedules)
+  for(let i=0; i<count; ++i){
+    upcomingCollectDates.push(upcomingCollectDatesGenerator.next())
+  }
+  return upcomingCollectDates
+}
+
