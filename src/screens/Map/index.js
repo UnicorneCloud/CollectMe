200cccebd61be3aef880150d547f5e0dfc53593d
lea <lea.marchand@hotmail.ca>
2019-03-09 17:08:47 -0500

    map put markers

--- src/screens/Map/index.js
+++ src/screens/Map/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import MapView from 'react-native-maps'
-import { Marker } from 'react-native-maps'
+import React from "react";
+import MapView from "react-native-maps";
+import { Marker, Callout } from "react-native-maps";
+import Drawer from "react-native-drawer";
+
 import {
   Platform,
   Image,
@@ -8,7 +10,9 @@ import {
   TouchableOpacity,
   Dimensions,
   FlatList,
-  View as RNView
+  Linking,
+  View as RNView,
+  Switch
 } from "react-native";
 import { connect } from "react-redux";
 import {
@@ -24,50 +28,178 @@ import {
   View,
   Spinner
 } from "native-base";
-import {askLocationPermission,fetchLocation, fetchEcocenters,updateLocation} from '../../actions/location'
+import {
+  askLocationPermission,
+  fetchLocation,
+  fetchEcocenters,
+  updateLocation
+} from "../../actions/location";
 import styles from "./styles";
 const deviceWidth = Dimensions.get("window").width;
 const headerLogo = require("../../../assets/header-logo.png");
+import { ecocenterMarkers, geoJsonToCoord } from "../../utils/GeoUtils";
+import composteData from "../../../data/composte.json";
+import { ListItem, SearchBar } from 'react-native-elements'
 
 class MapScreen extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: {
+        ecocenters: true,
+        composte: true
+      }
+    };
   }
-  componentDidUpdate = () => {
-    this.props.updateLocation()
-  }
+  componentDidMount = () => {
+    this.props.updateLocation();
+  };
+  openStore = (lng, lat, label) => {
+    const scheme = Platform.select({
+      ios: "maps:0,0?q=",
+      android: "geo:0,0?q="
+    });
+    const latLng = `${lat},${lng}`;
+    const url = Platform.select({
+      ios: `${scheme}${label}@${latLng}`,
+      android: `${scheme}${latLng}(${label})`
+    });
+    console.log(url);
+    Linking.openURL(url);
+  };
+  openFilter = () => {
+    this._drawer.open();
+  };
+  closeFilter = () => {
+    this._drawer.close();
+  };
   render() {
-    const {coords} = this.props.location
-    const{ latitude, longitude} = coords
-    console.log(coords)
+    const {
+      location: { coords },
+      ecocenters
+    } = this.props;
+    const { latitude, longitude } = coords;
+    const { filters } = this.state;
     return (
       <Container>
-      <Header>
-        <Left>
-          <Button transparent onPress={() => this.props.navigation.openDrawer()}>
-            <Icon active name="menu" />
-          </Button>
-        </Left>
-        <Body>
-          <Image source={headerLogo} style={styles.imageHeader} />
-        </Body>
-        <Right />
-      </Header>
-        <View style={{flex:1, backgroundColor: 'powderblue'}}>
- <MapView
- showsUserLocation
- style={styles.map}
-    initialRegion={{
-      latitude: latitude,
-      longitude: longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  >
-  </MapView>
-  </View>
-  </Container>
-    )
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon active name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Image source={headerLogo} style={styles.imageHeader} />
+          </Body>
+          <Right>
+            <TouchableOpacity
+              style={{ flexDirection: "row" }}
+              onPress={this.openFilter}
+            >
+              <Icon active name="funnel" />
+            </TouchableOpacity>
+          </Right>
+        </Header>
+        <View style={{ flex: 1, backgroundColor: "powderblue" }}>
+          <Drawer
+            ref={ref => (this._drawer = ref)}
+            type="overlay"
+            tapToClose={true}
+            side="right"
+            openDrawerOffset={100}
+            content={
+              <View style={{ flex: 1, backgroundColor: "white" }}>
+                <ListItem
+                  title="Montrer les écocentres"
+                  rightIcon={
+                    <Switch
+                      value={filters.ecocenters}
+                      onValueChange={e =>
+                        this.setState({
+                          filters: { ...filters, ecocenters: e }
+                        })
+                      }
+                    />
+                  }
+                  containerStyle={{
+                    borderBottomWidth: 1
+                  }}
+                />
+                <ListItem
+                  title="Montrer les compostes communautaires"
+                  rightIcon={
+                    <Switch
+                      value={filters.composte}
+                      onValueChange={e =>
+                        this.setState({ filters: { ...filters, composte: e } })
+                      }
+                    />
+                  }
+                  containerStyle={{
+                    borderBottomWidth: 1
+                  }}
+                />
+              </View>
+            }
+          >
+            <MapView
+              showsUserLocation
+              style={styles.map}
+              initialRegion={{
+                latitude: latitude || 0,
+                longitude: longitude || 0,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+              }}
+            >
+              {filters.ecocenters &&
+                ecocenterMarkers(ecocenters).map(marker => (
+                  <Marker
+                    pinColor="green"
+                    coordinate={marker.coord}
+                    title={marker.title}
+                    description={marker.description}
+                  >
+                    <Callout
+                      onPress={() =>
+                        this.openStore(
+                          marker.coord.longitude,
+                          marker.coord.latitude,
+                          marker.title
+                        )
+                      }
+                    />
+                  </Marker>
+                ))}
+              {filters.composte &&
+                composteData.composteCommunautaire.map(composte => {
+                  const coord = geoJsonToCoord(composte.geometry);
+                  return (
+                    <Marker
+                      pinColor="#8B4513"
+                      coordinate={coord}
+                      title={composte.nom + " - Composte communautaire"}
+                    >
+                      <Callout
+                        onPress={() =>
+                          this.openStore(
+                            coord.longitude,
+                            coord.latitude,
+                            composte.nom
+                          )
+                        }
+                      />
+                    </Marker>
+                  );
+                })}
+            </MapView>
+          </Drawer>
+        </View>
+      </Container>
+    );
   }
 }
 
@@ -83,4 +215,7 @@ const mapStateToProps = state => ({
   fetchedLocation: state.locationReducer.fetchedLocation,
   ecocenters: state.locationReducer.ecocenters
 });
-export default connect(mapStateToProps, bindAction)(MapScreen);
+export default connect(
+  mapStateToProps,
+  bindAction
+)(MapScreen);
