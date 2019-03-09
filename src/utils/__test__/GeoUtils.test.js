66dc06598626090c36744f8aa2dc542c4466bb83
Alexandre Tremblay <alexandre.tremblay@unicornpowered.com>
2019-03-08 22:36:12 -0500

    function getPeriod

--- src/utils/__test__/GeoUtils.test.js
+++ src/utils/__test__/GeoUtils.test.js
@@ -1,12 +1,12 @@
 import { WEEK_DAYS_MAP, createFrequence } from '../model';
-import { getFrequence } from '../GeoUtils';
+import { getFrequence, getPeriod } from '../GeoUtils';
 import {FEATURE_OBJECT_1, FEATURE_OBJECT_2, FEATURE_OBJECT_3} from './GeoUtilsDataset';
 
-const EXPECTED_FREQUENCE_1 = createFrequence(WEEK_DAYS_MAP.MONDAY, 1);
+const EXPECTED_FREQUENCE_1 = createFrequence([WEEK_DAYS_MAP.MONDAY], 1);
 const EXPECTED_FREQUENCE_2 = createFrequence([WEEK_DAYS_MAP.MONDAY, WEEK_DAYS_MAP.THURSDAY], 1);
-const EXPECTED_FREQUENCE_3 = createFrequence(WEEK_DAYS_MAP.MONDAY, 2);
+const EXPECTED_FREQUENCE_3 = createFrequence([WEEK_DAYS_MAP.MONDAY], 2);
 
-describe("when testing", () => {
+describe("when doing tdd", () => {
   describe("in a hackathon", () => {
     it("should work 1", () => {
       expect(getFrequence(FEATURE_OBJECT_1)).toBe(EXPECTED_FREQUENCE_1);
@@ -18,6 +18,20 @@ describe("when testing", () => {
 
     it("should work 3", () => {
       expect(getFrequence(FEATURE_OBJECT_3)).toBe(EXPECTED_FREQUENCE_3);
+    });
+  });
+
+  describe("for period", () => {
+    it("should 1", () => {
+      expect(getPeriod(FEATURE_OBJECT_1.properties.JOUR_COLLECTE)).toBe(1);
+    });
+
+    it("should 2", () => {
+      expect(getPeriod(FEATURE_OBJECT_2.properties.JOUR_COLLECTE)).toBe(1);
+    });
+
+    it("should 3", () => {
+      expect(getPeriod(FEATURE_OBJECT_3.properties.JOUR_COLLECTE)).toBe(2);
     });
   });
 });
--- src/utils/model.js
+++ src/utils/model.js
@@ -1,4 +1,5 @@
 export const WEEK_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
 export const WEEK_DAYS_MAP = {
   MONDAY: "Monday",
   TUESDAY: "Tuesday",
@@ -7,6 +8,21 @@ export const WEEK_DAYS_MAP = {
   FRIDAY: "Friday",
   SATURDAY: "Saturday",
   SUNDAY: "Sunday"
+};
+
+export const COLLECT_PERIOD_IN_DATA = {
+  SEMAINE: "chaque semaine",
+  BIMENSUEL: "chaque 2 semaines"
+};
+
+export const WEEK_DAYS_IN_DATA = {
+  MONDAY: "lundi",
+  TUESDAY: "mardi",
+  WEDNESDAY: "mercredi",
+  THURSDAY: "jeudi",
+  FRIDAY: "vendredi",
+  SATURDAY: "samedi",
+  SUNDAY: "dimanche"
 };
 
 export const createFrequence = (iterableOfDay, everyXWeek) => {
